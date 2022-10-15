daf4d3c038d47a443f6c8ad61351dba13a268859
a-s-t-e-y-a <krishnoit100@gmail.com>
2022-10-16 00:06:08 +0530

    changes in script added a root package.json file

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,17 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-app.use(cors({
-    origin: 'http://localhost:3001'
-}));
-const planetRouter =require('./routes/planets/planets.router')
+const path = require("path");
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+const planetRouter = require("./routes/planets/planets.router");
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 app.use(planetRouter);
 module.exports = app;
